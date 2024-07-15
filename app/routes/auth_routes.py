d56556997e246ea8732456b1263099f32883bdc4
wangwei02 <wangwei02@sunboxsoft.com>
2024-07-15 17:43:33 +0800

    1、登录成功缓存 2、获取用户修改

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,7 +1,11 @@
+import json
+
 from flask import Blueprint, request, jsonify
 from app.models import TestUser
 from flask_jwt_extended import create_access_token
 from app.services import UserService
+from app import redis_client
+from app.dto.user_dto import UserDTO
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -28,9 +32,16 @@ def login():
     email = data.get('email')
     pwd = data.get('password')
 
-    user =  UserService.login_user(email=email, password=pwd)
+    user = UserService.login_user(email=email, password=pwd)
+    id = user.id
     if user and user.check_password(pwd):
+        if(redis_client.get(f"user:{id}")):
+            user_dto = redis_client.get(f"user:{id}")
+        else:
+            user_dto = UserDTO(user)
+            user_json = json.dumps(user_dto.to_dict())
+            redis_client.set(f"user:{id}", user_json, ex=60*60)
         access_token = create_access_token(identity=email)
-        return jsonify({"token": access_token, "user": {"name": user.name, "email": user.email}}), 200
+        return jsonify({"token": access_token, "user": {"name": user_dto.name, "email": user_dto.email}}), 200
     else:
         return jsonify({"message": "Invalid credentials"}), 401
--- app/routes/test_user_routes.py
+++ app/routes/test_user_routes.py
@@ -1,5 +1,8 @@
+import json
+
 from flask import Blueprint, jsonify, request
 from app.services.user_service import UserService
+from app import redis_client
 
 test_user_bp = Blueprint('test_user_bp', __name__)
 
@@ -33,3 +36,17 @@ def delete_test_user(id):
         return jsonify({'message': 'User deleted successfully!'})
     else:
         return jsonify({'message': 'User not found'}), 404
+
+@test_user_bp.route('/test_users/<id>', methods=['GET'])
+def get_test_user(id):
+    try:
+        user_dto = redis_client.get(f"user:{id}")
+        user_json = user_dto.decode('utf-8')  # 解码为字符串
+        if user_json:
+            user_dict = json.loads(user_json)  # 将 JSON 字符串解析为字典
+            user_dict.pop('pwd', None)
+            return jsonify(user_dict), 200
+        else:
+            return jsonify({'message': 'User not found'}), 404
+    except Exception as e:
+        return jsonify({'message': str(e)}), 500
